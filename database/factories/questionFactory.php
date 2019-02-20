714b6a5db25f2997b234dc17494029c1702b29da
NathanABrooks <nathan.a.brooks1997@gmail.com>
2019-02-20 23:00:18 +0000

    Changed question factory to add '?' to end of questions

--- database/factories/questionFactory.php
+++ database/factories/questionFactory.php
@@ -18,13 +18,18 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(Question::class, function (Faker $faker) {
-	// Add more randomness to question length as variableNbWords only vairies by +/- 40%
+	// Add more randomness to question length as variableNbWords only vairies by +/- 40%.
 	$questionLength = $faker->numberBetween($min = 5, $max = 30);
+
+	// Replace the '.' at the end of the sentence with a '?'.
+	$sentence = $faker->sentence($nbWords = $questionLength, $variableNbWords = true);
+	$grammarless = rtrim($sentence, ".");
+	$question = $grammarless."?";
 
     return [
         'sessionID' => $faker->numberBetween($min = 1, $max = Session::count()),
         'userID' 	=> $faker->numberBetween($min = 1, $max = User::count()),
-        'question'  => $faker->sentence($nbWords = $questionLength, $variableNbWords = true),
+        'question'  => $question,
         'priority'  => $faker->numberBetween($min = 0, $max = 4),
     ];
 });
