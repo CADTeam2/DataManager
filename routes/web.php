5d0687a409831ab1e67c11101faccfb9d8353597
nathan.a.brooks1997@gmail.com <nathan.brooks@bluebaytravel.co.uk>
2019-02-24 19:30:44 +0000

    Added the API routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,93 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group([
+    'prefix' => 'api',
+], function() use ($router) {
+
+	// User API Routes:
+    $router->get('users', [
+        'uses' => 'UserController@showAllUsers',
+    ]);
+    $router->get('users/{userID}', [
+        'uses' => 'UserController@showUser',
+    ]);
+    $router->post('users', [
+        'uses' => 'UserController@create',
+    ]);
+    $router->delete('users/{userID}', [
+        'uses' => 'UserController@delete',
+    ]);
+    $router->put('users/{userID}', [
+    	'uses' => 'UserController@update',
+    ]);
+
+    // Event API Routes:
+    $router->get('events', [
+        'uses' => 'EventController@showAllEvents',
+    ]);
+    $router->get('events/{eventID}', [
+        'uses' => 'EventController@showEvent',
+    ]);
+    $router->post('events', [
+        'uses' => 'EventController@create',
+    ]);
+    $router->delete('events/{eventID}', [
+        'uses' => 'EventController@delete',
+    ]);
+    $router->put('events/{eventID}', [
+    	'uses' => 'EventController@update',
+    ]);
+
+    // Session API Routes:
+    $router->get('sessions', [
+        'uses' => 'SessionController@showAllSessions',
+    ]);
+    $router->get('sessions/{sessionID}', [
+        'uses' => 'SessionController@showSession',
+    ]);
+    $router->post('sessions', [
+        'uses' => 'SessionController@create',
+    ]);
+    $router->delete('sessions/{sessionID}', [
+        'uses' => 'SessionController@delete',
+    ]);
+    $router->put('sessions/{sessionID}', [
+    	'uses' => 'SessionController@update',
+    ]);
+
+    // Questions API Routes:
+    $router->get('questions', [
+        'uses' => 'QuestionController@showAllQuestions',
+    ]);
+    $router->get('questions/{questionID}', [
+        'uses' => 'QuestionController@showQuestion',
+    ]);
+    $router->post('questions', [
+        'uses' => 'QuestionController@create',
+    ]);
+    $router->delete('questions/{questionID}', [
+        'uses' => 'QuestionController@delete',
+    ]);
+    $router->put('questions/{questionID}', [
+    	'uses' => 'QuestionController@update',
+    ]);
+
+    // Attendance API Routes:
+    $router->get('attendances', [
+        'uses' => 'AttendanceController@showAllAttendances',
+    ]);
+    $router->get('attendances/{sessionID}/{userID}', [
+        'uses' => 'AttendanceController@showAttendance',
+    ]);
+    $router->post('attendances', [
+        'uses' => 'AttendanceController@create',
+    ]);
+    $router->delete('attendances/{sessionID}/{userID}', [
+        'uses' => 'AttendanceController@delete',
+    ]);
+    $router->put('attendances/{sessionID}/{userID}', [
+    	'uses' => 'AttendanceController@update',
+    ]);
+});
