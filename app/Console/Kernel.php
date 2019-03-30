50e74f66e93cf31b52271e70b3769def1c00dff8
NathanABrooks <nathan.a.brooks1997@gmail.com>
2019-03-30 17:36:01 +0000

    Removed unused files and folders from the live version

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,11 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
+/** 
+ * Do not delete this file.
+ * Even though currently empty, it is registered in bootstrap/app.php and the
+ * ConsoleKernel it extends is required by the application.
+ */
 class Kernel extends ConsoleKernel
 {
     /**
